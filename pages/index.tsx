773a839f63b9fd5b559ff8312c2629743c433675
mf-yechan-do <Yechan.do@mofl.ai>
2022-01-09 17:50:35 +0900

    CSS 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,35 @@
 import type {NextPage} from "next";
 import styles from "../styles/Home.module.css";
+import styled from 'styled-components';
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.logo}>
+    <LogoBox className={styles.logobox}>
+      <div>
       <img src="/logo.svg" alt="Vercel Logo" />
-    </div>
+      <div>준비중...</div>
+      </div>
+    </LogoBox>
   );
 };
 
 export default Home;
+
+const LogoBox = styled.div`
+  display: grid;
+  width: 100%;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+  padding: 0px;
+  margin: 0px;
+  img {
+    width: 500px;
+  }
+  div {
+    font-size: 1.25rem;
+    font-weight: 500;
+    text-align: center;
+    padding: 20px;
+  }
+`
